4fc9363ee8396a8b8cdfb3ecada325e64e1b370e
Brenton MOODLEY <bmoodley@e4r7p9.wethinkcode.co.za>
2018-06-09 13:03:13 +0200

    new main

--- main.cpp
+++ main.cpp
@@ -1,50 +1,12 @@
-#include "Player.hpp"
-#include <ncurses.h>
-#include <unistd.h>
+#include "Arena.hpp"
 
-int	main()
+int main()
 {
-	Player plr;
+	Arena gameArena;
 
-	int	quit = 0;
-	int	scrX = 0;
-	int	scrY = 0;
-	int	ch = 0;
+	gameArena.initialise();
+	gameArena.gameLoop();
+	gameArena.exit();
 
-	// init ncurses
-	initscr();
-	keypad(stdscr, TRUE);
-	noecho();
-	curs_set(FALSE);
-	raw();
-	getmaxyx(stdscr, scrY, scrX);
-	
-	// game loop
-	while (!quit){
-		clear();
-		box( stdscr, 0, 0);
-		mvprintw(plr.y, plr.x, ">");
-		refresh();
-		usleep(50000); // includes c library
-		ch = getch();
-		switch (ch) {
-			case KEY_LEFT:
-				plr.x -= 4;
-				break;
-			case KEY_RIGHT:
-				plr.x++;
-				break;
-			case KEY_UP:
-				plr.y--;
-				break;
-			case KEY_DOWN:
-				plr.y++;
-				break;
-			case 'q':
-				quit = 1;
-				break;
-		}
-	}
-	endwin();
 	return (0);
 }
--- main_eg.cpp
+++
@@ -1,8 +0,0 @@
-int main()
-{
-	Arena.init() //inits ncurses and constants
-	Arena.gameLoop();
-	Arena.quit();
-
-	return (0);
-}
